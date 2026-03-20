c37394a62ad07adcf1b440baac81654f92472c21
ranjitha_priya <ppranjithapriya@google.com>
2026-03-20 11:33:18 +0530

    fix: remove app.options incompatible with Express 5

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.options('/(.*)', cors()); // Handle preflight requests (Express 5 compatible)
+
 app.use(express.json());
 
 // Routes
